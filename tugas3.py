2d696440f2a0ade02fbf1661c403bdfce40b72a8
dewanggalisdi21 <dewanggaxd@gmail.com>
2024-11-06 18:57:55 +0700

    Add files via upload

---
+++ tugas3.py
@@ -0,0 +1,7 @@
+usiaAnda = int(input("Masukkan Usia Anda :"))
+if(usiaAnda >= 18):
+    print("Anda Sudah Dewasa")
+elif(usiaAnda >= 13 and usiaAnda <= 17):
+    print("Anda Masih Remaja")
+elif(usiaAnda <= 12):
+    print("Anda Masih Anak-Anak")
