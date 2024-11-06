136e9ce3b11d2433403e6d99b76c55ac95690a73
dewanggalisdi21 <dewanggaxd@gmail.com>
2024-11-06 18:58:20 +0700

    Add files via upload

---
+++ tugas2.py
@@ -0,0 +1,5 @@
+nilaiUjian = int(input("Masukkan Nilai Ujian :"))
+if(nilaiUjian > 75):
+    print("Selamat Anda Lulus...horeeeeee")
+elif(nilaiUjian < 65):
+    print("Yah Anda Tidak Lulus, silahkan merenungi nasib")
